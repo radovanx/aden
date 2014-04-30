d5e07b8b01ea55d23b2c07749dfb21001f497642
Radomir Bednar <radovanmail@gmail.com>
2014-04-30 18:32:17 +0200

    new commit

---
+++ wp-content/themes/wordpress-bootstrap-master/front-page.php
@@ -0,0 +1,300 @@
+<?php get_header(); ?>
+ 
+<div class="carousel slide" id="carousel-519866">
+    <ol class="carousel-indicators">
+        <li class="active" data-slide-to="0" data-target="#carousel-519866">
+        </li>
+        <li data-slide-to="1" data-target="#carousel-519866">
+        </li>
+        <li data-slide-to="2" data-target="#carousel-519866">
+        </li>
+    </ol>
+    <div class="carousel-inner">
+        <div class="item active">
+            <img alt="" src="<?php echo get_template_directory_uri() ?>/images/slide-1.jpg">
+            <div class="carousel-caption">
+                <h4>
+                    First Thumbnail label
+                </h4>
+                <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+            </div>
+        </div>
+        <div class="item">
+            <img alt="" src="<?php echo get_template_directory_uri() ?>/images/slide-1.jpg">
+            <div class="carousel-caption">
+                <h4>
+                    Second Thumbnail label
+                </h4>
+                <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+            </div>
+        </div>
+        <div class="item">
+            <img alt="" src="<?php echo get_template_directory_uri() ?>/images/slide-1.jpg">
+            <div class="carousel-caption">
+                <h4>
+                    Third Thumbnail label
+                </h4>
+                <p>
+                    Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
+                </p>
+            </div>
+        </div>
+    </div> <a class="left carousel-control" href="#carousel-519866" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-519866" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+</div>
+
+
+
+<div class="container">
+    <div class="row clearfix"> 
+
+        <div class="col-md-9 column"> 
+            <h3>Top projects</h3>
+        </div>
+
+        <div class="col-md-9 column"> 
+            <div class="row"><p> </p></div>
+            <div class="col-md-6 column">
+                <div class="thumbnail">
+                    <a class="" href="http://www.adenimmo.web-4-all.cz/program_page.html">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/choriner-hoefe_src_1.-w316-h0-p0-F-S1.jpg"/>
+                    </a>
+                    <div class="panel-body">
+                        <h4>Palais Varnhagen</h4>
+                    </div> 
+                    <div class="panel-body">
+                        <span class="propertyListBoxDataItemName"><strong>Type of property:</strong> Exclusive Apartments</span> 
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Address:</strong> Krausenstrasse 37 - Schützenstrasse 46, Berlin-Mitte</span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Price range:</strong> € 152.000 - € 822.500    <i class="fa fa-eur"></i>  
+                            <i class="fa fa-usd"></i> <i class="fa fa-gbp"></i>
+                        </span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Size range:</strong> 40 m² - 144 m²</span>
+
+                    </div>
+                    <div class="panel-body"> 
+                        Elegant apartments in Berlin's top location, near Checkpoint Charlie & the city's famous press quarters.  
+                        <br>
+                    </div>
+                    <div class="panel-body">		
+                        <p>
+                            <a class="btn btn-danger" href="http://www.adenimmo.web-4-all.cz/program_page.html">make an enquiry</a>
+                            <a class="btn btn-default" href="http://www.adenimmo.web-4-all.cz/program_page.html">» view details </a>
+                        </p>	
+                    </div>
+                </div>	
+            </div>
+            <div class="col-md-6 column">
+                <div class="thumbnail">
+                    <a class="" href="http://www.adenimmo.web-4-all.cz/program_page.html">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/choriner-hoefe_src_1.-w316-h0-p0-F-S1.jpg"/>
+                    </a>
+                    <div class="panel-body">
+                        <h4>Palais Varnhagen</h4>
+                    </div>
+
+                    <div class="panel-body">
+                        <span class="propertyListBoxDataItemName"><strong>Type of property:</strong> 	Exclusive Apartments</span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Address:</strong> 	Krausenstrasse 37 - Schützenstrasse 46, Berlin-Mitte</span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Price range:</strong> 	€ 152.000 - € 822.500    <i class="fa fa-eur"></i>  
+                            <i class="fa fa-usd"></i> <i class="fa fa-gbp"></i>
+                        </span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Size range:</strong> 	40 m² - 144 m²</span>
+                    </div>
+                    <div class="panel-body">
+
+                        Elegant apartments in Berlin's top location, near Checkpoint Charlie & the city's famous press quarters. 
+
+                        <br>
+                    </div>
+                    <div class="panel-body">		
+                        <p>
+                            <a class="btn btn-danger" href="http://www.adenimmo.web-4-all.cz/program_page.html">make an enquiry</a>
+                            <a class="btn btn-default" href="http://www.adenimmo.web-4-all.cz/program_page.html">» view details </a>
+                        </p>	
+                    </div>
+                </div>	
+            </div>
+
+            <div class="col-md-6 column">
+
+                <div class="thumbnail">
+                    <a class="" href="http://www.adenimmo.web-4-all.cz/program_page.html">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/choriner-hoefe_src_1.-w316-h0-p0-F-S1.jpg"/>
+                    </a>
+                    <div class="panel-body">
+                        <h4>Palais Varnhagen</h4>
+                    </div>
+
+                    <div class="panel-body">
+                        <span class="propertyListBoxDataItemName"><strong>Type of property:</strong> Exclusive Apartments</span>
+
+                        <br>	 
+                        <span class="propertyListBoxDataItemName"><strong>Address:</strong> Krausenstrasse 37 - Schützenstrasse 46, Berlin-Mitte</span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Price range:</strong> € 152.000 - € 822.500    <i class="fa fa-eur"></i>  
+                            <i class="fa fa-usd"></i> <i class="fa fa-gbp"></i>
+                        </span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Size range:</strong> 	40 m² - 144 m²</span>
+                    </div>
+                    <div class="panel-body">
+
+                        Elegant apartments in Berlin's top location, near Checkpoint Charlie & the city's famous press quarters. 
+
+                        <br>
+                    </div>
+
+
+                    <div class="panel-body">		
+                        <p>
+                            <a class="btn btn-danger" href="#">make an enquiry</a>
+                            <a class="btn btn-default" href="http://www.adenimmo.web-4-all.cz/program_page.html">» view details </a>
+                        </p>	
+                    </div>
+                </div>	
+            </div>
+
+            <div class="col-md-6 column">
+
+                <div class="thumbnail">
+                    <a class="" href="http://www.adenimmo.web-4-all.cz/program_page.html">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/choriner-hoefe_src_1.-w316-h0-p0-F-S1.jpg"/>
+                    </a>
+                    <div class="panel-body">
+                        <h4>Palais Varnhagen</h4>
+                    </div>
+
+                    <div class="panel-body">
+                        <span class="propertyListBoxDataItemName"><strong>Type of property:</strong> Exclusive Apartments</span>
+
+                        <br>	 
+                        <span class="propertyListBoxDataItemName"><strong>Address:</strong> Krausenstrasse 37 - Schützenstrasse 46, Berlin-Mitte</span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Price range:</strong> € 152.000 - € 822.500    <i class="fa fa-eur"></i>  
+                            <i class="fa fa-usd"></i> <i class="fa fa-gbp"></i>
+                        </span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Size range:</strong> 	40 m² - 144 m²</span>
+                    </div>
+                    <div class="panel-body">
+
+                        Elegant apartments in Berlin's top location, near Checkpoint Charlie & the city's famous press quarters. 
+
+                        <br>
+                    </div>
+
+
+                    <div class="panel-body">		
+                        <p>
+                            <a class="btn btn-danger" href="#">make an enquiry</a>
+                            <a class="btn btn-default" href="http://www.adenimmo.web-4-all.cz/program_page.html">» view details </a>
+                        </p>	
+                    </div>
+                </div>	
+            </div>
+
+            <div class="col-md-6 column">
+
+                <div class="thumbnail">
+                    <a class="" href="http://www.adenimmo.web-4-all.cz/program_page.html">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/choriner-hoefe_src_1.-w316-h0-p0-F-S1.jpg"/>
+                    </a>
+                    <div class="panel-body">
+                        <h4>Palais Varnhagen</h4>
+                    </div>
+
+                    <div class="panel-body">
+                        <span class="propertyListBoxDataItemName"><strong>Type of property:</strong> Exclusive Apartments</span>
+
+                        <br>	 
+                        <span class="propertyListBoxDataItemName"><strong>Address:</strong> Krausenstrasse 37 - Schützenstrasse 46, Berlin-Mitte</span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Price range:</strong> € 152.000 - € 822.500    <i class="fa fa-eur"></i>  
+                            <i class="fa fa-usd"></i> <i class="fa fa-gbp"></i>
+                        </span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Size range:</strong> 	40 m² - 144 m²</span>
+                    </div>
+                    <div class="panel-body">
+
+                        Elegant apartments in Berlin's top location, near Checkpoint Charlie & the city's famous press quarters. 
+
+                        <br>
+                    </div>
+
+
+                    <div class="panel-body">		
+                        <p>
+                            <a class="btn btn-danger" href="#">make an enquiry</a>
+                            <a class="btn btn-default" href="http://www.adenimmo.web-4-all.cz/program_page.html">» view details </a>
+                        </p>	
+                    </div>
+                </div>	
+            </div>
+
+            <div class="col-md-6 column">
+
+                <div class="thumbnail">
+                    <a class="" href="http://www.adenimmo.web-4-all.cz/program_page.html">
+                        <img src="<?php echo get_template_directory_uri() ?>/images/choriner-hoefe_src_1.-w316-h0-p0-F-S1.jpg"/>
+                    </a>
+                    <div class="panel-body">
+                        <h4>Palais Varnhagen</h4>
+                    </div>
+
+                    <div class="panel-body">
+                        <span class="propertyListBoxDataItemName"><strong>Type of property:</strong> Exclusive Apartments</span>
+
+                        <br>	 
+                        <span class="propertyListBoxDataItemName"><strong>Address:</strong> Krausenstrasse 37 - Schützenstrasse 46, Berlin-Mitte</span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Price range:</strong> € 152.000 - € 822.500    <i class="fa fa-eur"></i>  
+                            <i class="fa fa-usd"></i> <i class="fa fa-gbp"></i>
+                        </span>
+                        <br>
+                        <span class="propertyListBoxDataItemName"><strong>Size range:</strong> 	40 m² - 144 m²</span>
+                    </div>
+                    <div class="panel-body">
+
+                        Elegant apartments in Berlin's top location, near Checkpoint Charlie & the city's famous press quarters. 
+
+                        <br>
+                    </div>
+
+
+                    <div class="panel-body">		
+                        <p>
+                            <a class="btn btn-danger" href="#">make an enquiry</a>
+                            <a class="btn btn-default" href="http://www.adenimmo.web-4-all.cz/program_page.html">» view details </a>
+                        </p>	
+                    </div>
+                </div>	
+            </div>
+
+
+
+        </div> 
+        <div class="col-md-3 column"> 
+
+            <img src="<?php echo get_template_directory_uri() ?>/images/sidebar_left.png">
+
+        </div>	
+    </div>
+    <!-- FOOTER -->
+    <footer>
+        <p class="pull-right"><a href="#">Back to top</a></p>
+        <p>FOOTER<a href="http://www.bootply.com/62603"></a></p>
+    </footer>
+
+</div>
+</body>
+</html>
