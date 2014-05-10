a9aea7f6c27e86af53569101cbb7a917704fa02a
ales <ales@ales-PC>
2014-05-10 21:43:43 +0200

    Base my preference page

--- wp-content/themes/wordpress-bootstrap-master/template-buy_page.php
+++ wp-content/themes/wordpress-bootstrap-master/template-buy_page.php
@@ -1,9 +1,9 @@
-<?php 
+<?php
 /**
  * Template Name: Buy page
  */
-
-get_header(); ?>
+get_header();
+?>
 <div class="container">
     <div id="content" class="clearfix row">
 
@@ -32,13 +32,13 @@ get_header(); ?>
 
                     </article> <!-- end article -->
 
-                    
-                        <div class="border col-md-12 column border background contact_form_block">
-                            <h2 class="border-left uppercase"><?php _e('Contact Form', 'wpbootstrap') ?></h2>
-                            <span class="phone red bold"><i class="fa fa-phone"></i> +33 0632140564</span>
-                            <?php echo do_shortcode('[contact-form-7 id="4082" title="Buy page form - en"]') ?>
-                        </div>
-                    
+
+                    <div class="border col-md-12 column border background contact_form_block">
+                        <h2 class="border-left uppercase"><?php _e('Contact Form', 'wpbootstrap') ?></h2>
+                        <span class="phone red bold"><i class="fa fa-phone"></i> +33 0632140564</span>
+                        <?php echo do_shortcode('[contact-form-7 id="4082" title="Buy page form - en"]') ?>
+                    </div>
+
 
                 <?php endwhile; ?>
 
