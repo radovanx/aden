5e888a527c11493255f8c5892dc2589f16b672d1
ales <ales@ales-PC>
2014-05-13 15:45:40 +0200

    my preference, removing row, when remove apartment from preference

--- wp-content/plugins/estateprogram/public/assets/js/public.js
+++ wp-content/plugins/estateprogram/public/assets/js/public.js
@@ -11,18 +11,43 @@
                 flat_id: $(this).attr("data-flat_id")
             };
 
-            $.post(estateprogram.ajaxurl, data, function(response) {
-                if(1 == response){
-                    element.removeClass('fa-star-o blue').addClass('fa-star red');
-                } else {
-                    element.removeClass('fa-star red').addClass('fa-star-o blue');
-                }
-            }).fail(function() {
-                //alert( "error" );
-            }).always(function() {
-                //alert( "finished" );
-            });
+            var confirm_remove = $(this).hasClass("confirm-remove");
+            var remove_row = $(this).hasClass("remove-row");
+
+            if (confirm_remove) {
+                $('#removePreferenceModal').modal({backdrop: 'static', keyboard: false}).one('click', '#delete', function() {
+                    $.post(estateprogram.ajaxurl, data, function(response) {
+                        if (1 == response) {
+                            element.removeClass('fa-star-o blue').addClass('fa-star red');
+                        } else {
+                            if (remove_row) {
+                                element.closest('tr').remove();
+                            } else {
+                                element.removeClass('fa-star red').addClass('fa-star-o blue');
+                            }
+                        }
+                    }).fail(function() {
+                        //alert( "error" );
+                    })
+                });
+            } else {
+                $.post(estateprogram.ajaxurl, data, function(response) {
+                    if (1 == response) {
+                        element.removeClass('fa-star-o blue').addClass('fa-star red');
+                    } else {
+                        if (remove_row) {
+                            element.closest('tr').remove();
+                        } else {
+                            element.removeClass('fa-star red').addClass('fa-star-o blue');
+                        }
+                    }
+                }).fail(function() {
+                    //alert( "error" );
+                })
+            }
         });
     });
 
 }(jQuery));
+
+
