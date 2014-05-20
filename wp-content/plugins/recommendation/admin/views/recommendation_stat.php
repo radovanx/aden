7f36c495edb322a6d6820dfd6c4be98de7e9b364
ales <ales@ales-PC>
2014-05-20 16:08:21 +0200

    reservation document

--- wp-content/plugins/recommendation/admin/views/recommendation_stat.php
+++ wp-content/plugins/recommendation/admin/views/recommendation_stat.php
@@ -42,16 +42,21 @@
                         $props = unserialize($row->product);
                         $user_info = get_userdata($row->user_id);
                         ?>
-                        <tr class="<?php echo 0 == $i%2 ? 'even' : 'odd' ?>">
-                            <td><?php echo $user_info->user_email ?></td>
+                        <tr class="<?php echo 0 == $i % 2 ? 'even' : 'odd' ?>">
+                            <td>
+                                <a href="/wp-admin/user-edit.php?user_id=<?php echo (int) $row->user_id ?>" style="text-decoration: none;">
+                                    <span class="wp-menu-image dashicons-before dashicons-admin-users"></span>
+                                </a>
+                                <?php echo $user_info->user_email ?>
+                            </td>
                             <td><?php echo esc_attr($row->receiver) ?></td>
                             <td><?php echo esc_attr($row->fdate) ?></td>
                             <td class="color-brown"><?php echo esc_attr($props['verwaltung_techn|objektnr_extern']) ?></td>
                             <td><?php echo esc_attr($props['freitexte|objekttitel']) ?></td>
                         </tr>
-                    <?php 
-                    $i++;
-                    endforeach; 
+                        <?php
+                        $i++;
+                    endforeach;
                     ?>
                 </tbody>
             </table>
