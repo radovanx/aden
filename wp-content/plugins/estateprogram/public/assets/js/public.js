210edff1fa62a09a41d20a1338ee08ef5c9003ad
Radomir Bednar <radovanmail@gmail.com>
2014-05-23 20:29:18 +0200

    images update

--- wp-content/plugins/estateprogram/public/assets/js/public.js
+++ wp-content/plugins/estateprogram/public/assets/js/public.js
@@ -2,17 +2,13 @@
     "use strict";
 
     $(function() {
-        $('.add-to-preference').live("click", function() {
-
-            var element = $(this).find('i');
-            
-            var this_element = $(this);
-
+        $('.add-to-preference').live("click", function() { 
+            var element = $(this).find('i'); 
+            var this_element = $(this); 
             var data = {
                 action: 'add_to_preference',
                 flat_id: $(this).attr("data-flat_id")
-            };
-
+            }; 
             var confirm_remove = $('.apartment-list').hasClass("confirm-remove");
             var remove_row = $('.apartment-list').hasClass("remove-favorite-row");
 
@@ -25,15 +21,23 @@
                             if (remove_row) {
                                 console.log('remove');
                                 this_element.closest('.apartment-row').remove();
+                                 
+                                 
                             } else {
                                 element.removeClass('fa-star red').addClass('fa-star-o blue');
                             }
                         }
                     }).fail(function() {
-                        //alert( "error" );
+                
                     })
                 });
-            } else {
+            } 
+            
+            
+            
+            
+            
+            else {
                 $.post(estateprogram.ajaxurl, data, function(response) {
                     if (1 == response) {
                         element.removeClass('fa-star-o blue').addClass('fa-star red');
@@ -49,12 +53,10 @@
                         }
                     }
                 }).fail(function() {
-                    //alert( "error" );
                 })
             }
         });
-    });
-
+    }); 
 }(jQuery));
 
 
