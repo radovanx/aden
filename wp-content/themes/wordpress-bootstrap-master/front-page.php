977cbc08f916ba83c79cd32b9d949a2ac95722c0
ales <ales@ales-PC>
2014-05-28 14:40:34 +0200

    mailchimp oprava formu

--- wp-content/themes/wordpress-bootstrap-master/front-page.php
+++ wp-content/themes/wordpress-bootstrap-master/front-page.php
@@ -124,25 +124,27 @@ $query = new WP_Query($args);
                 </div>
                 <div class="col-md-12 column border newsletter-background contact_form_block">
                     <h2 class="border-left"><?php _e("NEWSLETTER", "wpbootstrap"); ?></h2>
-                        <!-- Begin MailChimp Signup Form -->
-                        <div id="mc_embed_signup">
-                            <form action="http://aden-immo.us3.list-manage.com/subscribe/post?u=17d843b86bdd3339c6dbc1da5&amp;id=49b2f803a7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>                                
-                                <div class="form-group">
-                                    <label for="mce-EMAIL">Email Address </label>
-                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                    <!-- Begin MailChimp Signup Form -->
+                    <div id="mc_embed_signup">
+                        <form action="http://aden-immo.us3.list-manage.com/subscribe/post?u=17d843b86bdd3339c6dbc1da5&amp;id=49b2f803a7" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>                                
+                            <div class="form-group">
+
+                                <input type="email" value="" name="EMAIL" class="pull-left required email form-control input-lg" id="mce-EMAIL" placeholder="<?php _e('Insert your email', 'wpbootstrap') ?>">
+                                <input type="submit" value="<?php _e('OK', 'wpbootstrap') ?>" name="subscribe" id="mc-embedded-subscribe" class="button pull-left btn-primary">
+
                                 <div class="clearfix"></div>
                             </div>
-                                <div id="mce-responses" class="clear">
-                                    <div class="response" id="mce-error-response" style="display:none"></div>
-                                    <div class="response" id="mce-success-response" style="display:none"></div>
-                                </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                                <div style="position: absolute; left: -5000px;"><input type="text" name="b_17d843b86bdd3339c6dbc1da5_49b2f803a7" tabindex="-1" value=""></div>
-                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-                            </form>
-                        </div>
-                        <!--End mc_embed_signup-->                        
+                            <div id="mce-responses" class="clear">
+                                <div class="response" id="mce-error-response" style="display:none"></div>
+                                <div class="response" id="mce-success-response" style="display:none"></div>
+                            </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+                            <div style="position: absolute; left: -5000px;"><input type="text" name="b_17d843b86bdd3339c6dbc1da5_49b2f803a7" tabindex="-1" value=""></div>
+                            <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                        </form>
+                    </div>
+                    <!--End mc_embed_signup-->                        
 
-      
+
 
                 </div>
             </div>
