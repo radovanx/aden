a8fcb359b432c6bcb3fac188132fd3067aff5679
Radomir Bednar <radovanmail@gmail.com>
2014-06-11 12:48:58 +0200

    offen

--- wp-content/themes/wordpress-bootstrap-master/our-product-page.php
+++ wp-content/themes/wordpress-bootstrap-master/our-product-page.php
@@ -130,14 +130,16 @@ get_header();
                                     //$status = isset($prop['zustand_angaben|verkaufstatus|stand']) ? esc_attr($prop['zustand_angaben|verkaufstatus|stand']) : "-";
                                     
                                     $status = statusL($prop); 
+                                    
+                                    if( $status == 'OFFEN' )
+                                     {
+                                       $status = '';
+                                     }  
                                    
                                     $reference = isset($prop['verwaltung_techn|objektnr_extern']) ? esc_attr($prop['verwaltung_techn|objektnr_extern']) : "-";                                                    
                                     $data_object.="{city:\"" . $city . "\",name:\"" . $name . "\", district:\"" . $district . "\", hnumber:" . $hnumber . ",  street:\"" . $street . "\", area:" . $area . ", zip:" . $zip . ", rooms:" . $rooms . ", flatnum:\"" . $flat_num . "\", references:\"" . $reference . "\",price: " . esc_attr($prop['preise|kaufpreis']) . ", fprice: \"" . esc_attr(price_format($prop['preise|kaufpreis'])) . "\" ,pricem: ".$pricem.", fpricem: \"" . price_format($pricem) . "\"  , url:\"" . $url . "\", image_url:  \"" . $url_image . "\", floor:" . $floor . ", rstatus: \"" .$rental_status."\", status: \"" .$status."\", favorite: \"" .$favor."\",type: \"" .$term."\", idval: ".$idval." },";
                                    
-                                     if( $status == 'OFFEN' )
-                                     {
-                                       $status = '';
-                                     }  
+                                     
                                      
                                     if ($i < 10):
                                         ?>  
@@ -262,13 +264,8 @@ get_header();
 <script src="<?php bloginfo('template_directory'); ?>/library/js/pourover.js"></script> 
 <script>  
         var datatable = <?php echo $data_object; ?>;
-        var collection = new PourOver.Collection(datatable); 
-        
-        
-        jQuery("form").on("submit", function(event) { 
-            
-        
-            
+        var collection = new PourOver.Collection(datatable);  
+        jQuery("form").on("submit", function(event) {  
         event.preventDefault();   
         var values = {}; 
         jQuery.each(jQuery('form').serializeArray(), function(i, field) { 
