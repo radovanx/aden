db263ce2cced01b6c186d5867bf547dc64620807
ala <ala@ala-Inspiron-3847>
2014-07-29 17:17:11 +0200

    get_all_flats($lang, $limit = null, $offset = null)  - podpora selektu budovy

--- wp-content/plugins/estateprogram/public/class-estateprogram.php
+++ wp-content/plugins/estateprogram/public/class-estateprogram.php
@@ -931,91 +931,8 @@ class EstateProgram {
     /**
      *
      */
-    public static function get_all_flats($program_id, $lang, $limit = null, $offset = null) {
+    public static function get_all_flats($lang, $limit = null, $offset = null) {
         global $wpdb;
-
-        /*
-          $sql = "
-          SELECT
-          p.ID,
-          m.meta_value as prop,
-          IFNULL(up.flat_id, 0) as is_favorite,
-          p.post_name as slug
-          FROM
-          wp_posts AS p
-          JOIN
-          wp_postmeta as m
-          ON
-          m.post_id = p.ID
-          LEFT JOIN
-          user_preference	AS up
-          ON
-          up.flat_id = p.ID
-          LEFT JOIN
-          wp_users AS u
-          ON
-          up.user_id = u.ID AND u.ID = " . (int) get_current_user_id() . "
-          WHERE
-          m.meta_key = 'flat_props_" . esc_sql($lang) . "'
-          AND
-          p.post_type = 'flat'
-          AND
-          p.post_status = 'publish'
-          "; */
-
-        /*
-          $sql = "SELECT
-          p.ID,
-          m.meta_value as prop,
-          IFNULL(up.flat_id, 0) as is_favorite,
-          p.post_name as slug,
-          tt.term_taxonomy_id,
-          t.term_id,
-          t.name AS term_name,
-          a2p.program_id as program_id
-          FROM
-          wp_posts AS p
-          JOIN
-          wp_postmeta as m
-          ON
-          m.post_id = p.ID
-          JOIN
-          apartment2program AS a2p
-          ON
-          a2p.apartment_id = p.ID
-          JOIN
-          wp_posts AS program
-          ON
-          a2p.program_id = program.ID
-          LEFT JOIN
-          wp_term_relationships AS tr
-          ON
-          program.ID = tr.object_id
-          LEFT JOIN
-          wp_term_taxonomy AS tt
-          ON
-          tt.term_taxonomy_id = tr.term_taxonomy_id AND tt.taxonomy = 'type_of_accommodation'
-          LEFT JOIN
-          wp_terms AS t
-          ON
-          t.term_id = tt.term_id
-          LEFT JOIN
-          user_preference	AS up
-          ON
-          up.flat_id = p.ID
-          LEFT JOIN
-          wp_users AS u
-          ON
-          up.user_id = u.ID AND u.ID = " . (int) get_current_user_id() . "
-          WHERE
-          m.meta_key = 'flat_props_" . esc_sql($lang) . "'
-          AND
-          p.post_type = 'flat'
-          AND
-          p.post_status = 'publish'
-          GROUP BY
-          p.ID";
-         */
 
         $sql = "SELECT
                 p.ID,
@@ -1029,11 +946,11 @@ class EstateProgram {
                 wp_postmeta as m
             ON
                 m.post_id = p.ID
-            JOIN
+            LEFT JOIN
                 apartment2program AS a2p
             ON
               a2p.apartment_id = p.ID
-            JOIN
+            LEFT JOIN
               wp_posts AS program
             ON
               a2p.program_id = program.ID
@@ -1077,11 +994,11 @@ class EstateProgram {
                 wp_postmeta as m
             ON
                 m.post_id = p.ID
-            JOIN
+            LEFT JOIN
                 apartment2program AS a2p
             ON
               a2p.apartment_id = p.ID
-            JOIN
+            LEFT JOIN
               wp_posts AS program
             ON
               a2p.program_id = program.ID
