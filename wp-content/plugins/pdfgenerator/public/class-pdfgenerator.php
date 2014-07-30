e59eff82cb15067a22c333c75e157a5d97c81a8c
Radomir Bednar <radovanmail@gmail.com>
2014-07-30 16:31:27 +0200

    update email

--- wp-content/plugins/pdfgenerator/public/class-pdfgenerator.php
+++ wp-content/plugins/pdfgenerator/public/class-pdfgenerator.php
@@ -104,11 +104,8 @@ class pdfgenerator {
             //'show_ui' => false,
             'public' => true,
             'supports' => array(
-                //'thumbnail',
-                'title',
-                'editor',
-            //'excerpt',
-            //'author'
+            'title',
+            'editor',         
             ),
         );
 
@@ -185,7 +182,6 @@ class pdfgenerator {
                 exit;
             }
         }
-
         // pdf prezentace produktu
         if (isset($q['action']) && 'generate-pdf' == $q['action']) {
             if (!empty($q['language'])) {
@@ -345,7 +341,7 @@ class pdfgenerator {
         } 
                
         $lang = $_POST['lang'];
-        //$props = get_post_meta($product->ID, 'flat_props_' . $lang, true);
+                    
 
         $props = get_props($product->ID, $lang); 
         $this->override_locale($lang); 
@@ -353,10 +349,7 @@ class pdfgenerator {
         $mpdf = $this->create_html2pdf($product, $props); 
         $presentation_string = $mpdf->Output('', 'S');
         $attachment = chunk_split(base64_encode($presentation_string));
-
-        //$user_data = get_userdata(get_current_user_id());
-        //$mailto = $user_data->user_email;
-        //$mailto = 'root@localhost';
+                    
 
         global $current_user;
         get_currentuserinfo();
@@ -374,9 +367,7 @@ class pdfgenerator {
         $message = 'Guten Tag,<br>'.$from_name.', Partner von Immoneda.com, empfiehlt Ihnen eine Immobilie. Diese finden sie im Anhang.';    
         $message .= 'Wir hoffen, dass ihnen das Objekt gefällt.<br>Mit freundlichen Grüßen,<br>Ihr Immoneda-Team';    
         }
-        /*$message. = strip_tags($_POST['receiver_message']);
-        $message. = str_replace("\n", "\r\n", $message);    
-        */           
+                    
         $from_mail = $current_user->user_email;
         $reply_to = $current_user->user_email; 
         if (!empty($props['freitexte|objekttitel'])) {
@@ -390,11 +381,10 @@ class pdfgenerator {
             $filename = $props['verwaltung_techn|objektnr_extern'];
         } 
         $filename = $filename . '.pdf'; 
-        // $message = esc_attr($message);
-// a random hash will be necessary to send mixed content
-        $separator = md5(time());
-// carriage return type (we use a PHP end of line constant)
+                    
+        $separator = md5(time());          
         $eol = PHP_EOL;
+                    
 // main header 
         $from_name = "=?UTF-8?B?" . base64_encode($from_name) . "?="; 
         $headers = "From: " . $from_name . " <" . $from_mail . "> " . $eol;
@@ -411,6 +401,7 @@ class pdfgenerator {
         ob_start();
         include get_template_directory() . "/email_template/template.php";
         $content = ob_get_contents();
+ 
         ob_end_clean();        
         
         $body .= "Content-Transfer-Encoding: 8bit" . $eol . $eol;
@@ -432,9 +423,7 @@ class pdfgenerator {
             _e('Email could not be sent, please contact administrator of this server.', $this->plugin_slug);
             die();
         }
-
-        // ulozim zaznam
-
+                    
         global $wpdb;
 
         $program_id = EstateProgram::flat_program_id($product->ID);
